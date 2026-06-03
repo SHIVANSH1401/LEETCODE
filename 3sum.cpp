63dd89d1adca7b9dfd4191b7570b0ba3a90e3d97
SHIVANSH1401 <Sshivansh.ssingh14@gmail.com>
2026-06-03 23:50:08 +0530

    Added code for 3 sum

---
+++ 3sum.cpp
@@ -0,0 +1,33 @@
+/*This solution uses the Sorting + Two Pointer technique. After sorting the array, one element is fixed at a time, 
+and two pointers are used to efficiently find the other two numbers that make the sum equal to zero while avoiding duplicate triplets.*/
+
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int j = i + 1, k = nums.size() - 1;
+
+            while (j < k) {
+                long long sum = nums[i] + nums[j] + nums[k];
+
+                if (sum < 0) j++;
+                else if (sum > 0) k--;
+                else {
+                    ans.push_back({nums[i], nums[j], nums[k]});
+                    j++;
+                    k--;
+
+                    while (j < k && nums[j] == nums[j - 1]) j++;
+                    while (j < k && nums[k] == nums[k + 1]) k--;
+                }
+            }
+        }
+        return ans;
+    }
+};
