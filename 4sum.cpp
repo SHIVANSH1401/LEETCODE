4c9a15ecd38cf6a4eb06111b4f21a823f7d00312
SHIVANSH1401 <Sshivansh.ssingh14@gmail.com>
2026-06-07 22:38:12 +0530

    Added code for 4 sum

---
+++ 4sum.cpp
@@ -0,0 +1,48 @@
+/*This solution first sorts the array and fixes the first two numbers using nested loops. 
+It then uses the two-pointer technique to find the remaining two numbers, 
+efficiently generating all unique quadruplets whose sum equals the target while avoiding duplicates.*/
+
+
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<vector<int>> ans;
+
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            for (int j = i + 1; j < n; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+
+                int k = j + 1;
+                int l = n - 1;
+
+                while (k < l) {
+                    long long sum = (long long)nums[i] + nums[j]
+                                  + nums[k] + nums[l];
+
+                    if (sum == target) {
+                        ans.push_back({nums[i], nums[j], nums[k], nums[l]});
+
+                        k++;
+                        l--;
+
+                        while (k < l && nums[k] == nums[k - 1]) k++;
+                        while (k < l && nums[l] == nums[l + 1]) l--;
+                    }
+                    else if (sum < target) {
+                        k++;
+                    }
+                    else {
+                        l--;
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+};
